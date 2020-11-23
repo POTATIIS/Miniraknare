8082970f72b6d55a3ce2d851e2d31027d17a5e23
POTATIIS <alexander.martinsson@elev.ga.ntig.se>
2020-11-23 14:26:44 +0100

    Meny Loop

---
+++ Miniräknare.py
@@ -0,0 +1,35 @@
+
+print("Välj alternativ. ")
+print("1.plus")    
+print("2.minus")
+print("3.gånger")
+print("4.delat")
+print("5.avsluta")
+
+while True:
+    alternativ = input("Välj alternativ: ")
+
+    if alternativ in ('1', '2', '3', '4'):
+        nummer1 = float(input("Välj den första siffran: "))
+        nummer2 = float(input("Välj den andra siffran: "))
+
+    if alternativ == '5':
+        print("Hej Då")
+        break
+
+    elif alternativ == '1':
+        print(nummer1, "+", nummer2, "=", plus(nummer1, nummer2))        
+
+    elif alternativ == '2':
+        print(nummer1, "-", nummer2, "=", minus(nummer1, nummer2))        
+
+    elif alternativ == '3':
+        print(nummer1, "*", nummer2, "=", gånger(nummer1, nummer2))
+
+    elif alternativ == '4':
+        print(nummer1, "/", nummer2, "=", delat(nummer1, nummer2))
+        break
+    
+    else:
+        print("Försök igen")
+  
